8ddcdffeb1470f9d02c28b599bb3fe587d517c06
Nond <Nond@nhac.attlocal.net>
2017-03-25 19:34:13 -0500

    added trial accelerometer

--- Swift-HAR/ViewController.swift
+++ Swift-HAR/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 import CoreMotion
 
+let manager = CMMotionManager()
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var accelXText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let manager = CMMotionManager()
         if manager.isGyroAvailable && manager.isAccelerometerAvailable && manager.isDeviceMotionAvailable {
-            manager.startAccelerometerUpdates()
+            //Set sensor data updates to 0.1 seconds
+            manager.accelerometerUpdateInterval = 0.1
+            manager.gyroUpdateInterval = 0.1
+            //Enable data updates from sensors
+            //manager.startAccelerometerUpdates()
             manager.startGyroUpdates()
-            
+            manager.startAccelerometerUpdates(to: .main) {
+                [weak self] (data: CMAccelerometerData?, error: Error?) in
+                if (data?.acceleration) != nil {
+                    self?.accelXText.text = String(format: "%.2f", (manager.accelerometerData?.acceleration.x)!)
+                }
+            }
+            /*
+            let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: Selector("stateUpdate"), userInfo: nil, repeats: true)
+            */
         }
     }
 
@@ -26,6 +41,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    func stateUpdate() {
+        if manager.accelerometerData != nil {
+            accelXText.text = String(format: "%.2f", (manager.accelerometerData?.acceleration.x)!)
+        }
+    }
 
 }
