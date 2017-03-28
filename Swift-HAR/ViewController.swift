7a672120eeed05f65386baa9318f1ed5bcbd6e2d
Nond <Nond@nhac.attlocal.net>
2017-03-27 21:05:26 -0500

    Fixed background color issue and added gyro data

--- Swift-HAR/ViewController.swift
+++ Swift-HAR/ViewController.swift
@@ -14,29 +14,44 @@ let manager = CMMotionManager()
 class ViewController: UIViewController {
 
     @IBOutlet weak var accelXText: UITextField!
-    
     @IBOutlet weak var accelYText: UITextField!
-  
     @IBOutlet weak var accelZText: UITextField!
+    @IBOutlet weak var gyroXText: UITextField!
+    @IBOutlet weak var gyroYText: UITextField!
+    @IBOutlet weak var gyroZText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        accelXText.delegate = self
+        accelYText.delegate = self
+        accelZText.delegate = self
+        definesPresentationContext = true
+        
         if manager.isGyroAvailable && manager.isAccelerometerAvailable && manager.isDeviceMotionAvailable {
             //Set sensor data updates to 0.1 seconds
             manager.accelerometerUpdateInterval = 0.1
             manager.gyroUpdateInterval = 0.1
             //Enable data updates from sensors
             //manager.startAccelerometerUpdates()
-            manager.startGyroUpdates()
+            manager.startGyroUpdates(to: .main) {
+                [weak self] (data: CMGyroData?, error: Error?) in
+                if (data?.rotationRate) != nil {
+                    self?.gyroXText.text = "Gyro-X: " + String(format: "%.2f", (manager.gyroData?.rotationRate.x)!)
+                    self?.gyroYText.text = "Gyro-Y: " + String(format: "%.2f", (manager.gyroData?.rotationRate.y)!)
+                    self?.gyroZText.text = "Gyro-Z: " + String(format: "%.2f", (manager.gyroData?.rotationRate.z)!)
+                }
+            }
+            
             manager.startAccelerometerUpdates(to: .main) {
                 [weak self] (data: CMAccelerometerData?, error: Error?) in
                 if (data?.acceleration) != nil {
-                    self?.accelXText.text = String(format: "%.2f", (manager.accelerometerData?.acceleration.x)!)
-                    self?.accelYText.text = String(format: "%.2f", (manager.accelerometerData?.acceleration.y)!)
-                    self?.accelZText.text = String(format: "%.2f", (manager.accelerometerData?.acceleration.z)!)
+                    self?.accelXText.text = "Accel-X: " + String(format: "%.2f", (manager.accelerometerData?.acceleration.x)!)
+                    self?.accelYText.text = "Accel-Y: " + String(format: "%.2f", (manager.accelerometerData?.acceleration.y)!)
+                    self?.accelZText.text = "Accel-Z: " + String(format: "%.2f", (manager.accelerometerData?.acceleration.z)!)
                 }
             }
+            
             /*
             let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: Selector("stateUpdate"), userInfo: nil, repeats: true)
             */
@@ -53,4 +68,12 @@ class ViewController: UIViewController {
         }
     }*/
 
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 }
